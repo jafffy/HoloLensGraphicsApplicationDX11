be166cb06d02e412dc426907c89310e9a7684138
Jaewon Choi <jainersoer@gmail.com>
2017-03-22 16:32:42 +0900

    Add ReactiveFrameRateControlExperiments

---
+++ FrameRateControlExperimentTest/ReactiveFrameRateControlExperimentCpp/pch.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <agile.h>
+#include <array>
+#include <d2d1_2.h>
+#include <d3d11_4.h>
+#include <DirectXColors.h>
+#include <dwrite_2.h>
+#include <map>
+#include <mutex>
+#include <wincodec.h>
+#include <WindowsNumerics.h>
+#include <windows.h>
+#include <tchar.h>
+#include <stdio.h>
+#include <strsafe.h>
+#pragma comment(lib, "User32.lib")
+#include <string>
